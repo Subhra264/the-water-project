b1a3a43a1dbd476963e2f498d5e077c58ce728ad
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-06-12 10:31:39 +0530

    hide secret key and add requirements.txt

--- server/the_water_project/settings.py
+++ server/the_water_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c)+fu_juydq4xjtd*e)a5gvbst(nxg@ngif300md!ius0r@67('
+SECRET_KEY = os.environ.get('SECRET_KEY', 'mongohahahah')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
