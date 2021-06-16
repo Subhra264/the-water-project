e17ac5b9ee7e8fbda5d020f17025b1a1d9b1ce96
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-06-16 16:26:38 +0530

    Add SubNavBar

--- client/src/components/SubNavBar/SubNavBar.jsx
+++ client/src/components/SubNavBar/SubNavBar.jsx
@@ -1,22 +1,39 @@
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
+import './SubNavBar.scss';
 
 export default function SubNavBar(props) {
+    const offsetWidths = useRef([]);
+    const linkRefs = useRef([useRef(null), useRef(null), useRef(null), useRef(null)]);
+
     const barItems = [
-        <div className='sub-navbar-item' key='home'>
-            <Link to='/home'>Home</Link>
-        </div>,
-        <div className='sub-navbar-item' key='problems'>
-            <Link to='/problems'>Problems</Link>
-        </div>,
-        <div className='sub-navbar-item' key='solutions'>
-            <Link to='/solutions'>Solutions</Link>
-        </div>,
-        <div className='sub-navbar-item' key='discussion'>
-            <Link to='/discussion'>Discussion</Link>
-        </div>
+        ['/', 'Home'],
+        ['/problems', 'Problems'],
+        ['/solutions', 'Solutions'],
+        ['/discussion', 'Discussion'],
     ];
 
+    useEffect(() => {
+        offsetWidths.current[0] = linkRefs.current[0].current.offsetWidth;
+        offsetWidths.current[1] = linkRefs.current[1].current.offsetWidth;
+        offsetWidths.current[2] = linkRefs.current[2].current.offsetWidth;
+        offsetWidths.current[3] = linkRefs.current[3].current.offsetWidth;
+        console.log(linkRefs.current);
+    }, [offsetWidths, linkRefs]);
+
     return (
-        barItems.map(barItem => barItem)
+        <div className='sub-navbar-container'>
+            <div className='sub-navbar'>
+                {console.log('I am rendered again')}
+                {
+                    barItems.map((link, index) => (
+                        <div className='sub-navbar-item' key={link[1]} ref={linkRefs.current[index]} >
+                            <Link to={link[0]}>{link[1]}</Link>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className='selected-bar'></div>
+        </div>
     )
 }
