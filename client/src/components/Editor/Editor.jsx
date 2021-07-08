c48cb516d1cb16d50e3ebd1ec219471975fb78ab
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-07-08 21:36:48 +0530

    Add TinyMCE Editor

---
+++ client/src/components/Editor/Editor.jsx
@@ -0,0 +1,43 @@
+import { Editor as TinyMceEditor} from '@tinymce/tinymce-react';
+import { useRef } from 'react';
+import 'tinymce/tinymce';
+import 'tinymce/icons/default';
+import 'tinymce/themes/silver';
+import 'tinymce/plugins/paste';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/table';
+import 'tinymce/skins/ui/oxide/skin.min.css';
+import 'tinymce/skins/ui/oxide/content.min.css';
+import 'tinymce/skins/content/default/content.min.css';
+
+export default function Editor (props) {
+    const editorRef = useRef(null);
+
+    return (
+        <div className="editor-container">
+            <TinyMceEditor 
+                // tinymceScriptSrc='node_modules/tinymce/tinymce.min.js'
+                onInit={(ev, editor) => editorRef.current = editor}
+                initialValue="<p>This is the initial content of the editor.</p>"
+                init={{
+                  height: props.height? props.height : 500,
+                  menubar: true,
+                  plugins: [
+                    'advlist autolink lists link image charmap print preview anchor',
+                    'searchreplace visualblocks code fullscreen',
+                    'insertdatetime media table paste code help wordcount'
+                  ],
+                  toolbar: 'undo redo | formatselect | ' +
+                  'bold italic backcolor | alignleft aligncenter ' +
+                  'alignright alignjustify | bullist numlist outdent indent | ' +
+                  'removeformat | help',
+                  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                }}
+                // value={contentEditor}
+                // onEditorChange={this.handleEditorChange}
+                
+            />
+        </div>
+    );
+}
