a7172f4a228c4e334db07c43bf5a56de0d495900
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-07-15 18:24:39 +0530

    Add CKEditor component and stylesheet

--- client/src/components/Editor/Editor.jsx
+++ client/src/components/Editor/Editor.jsx
@@ -1,42 +1,41 @@
-import { Editor as TinyMceEditor} from '@tinymce/tinymce-react';
-import { useRef } from 'react';
-import 'tinymce/tinymce';
-import 'tinymce/icons/default';
-import 'tinymce/themes/silver';
-import 'tinymce/plugins/paste';
-import 'tinymce/plugins/link';
-import 'tinymce/plugins/image';
-import 'tinymce/plugins/table';
-import 'tinymce/skins/ui/oxide/skin.min.css';
-import 'tinymce/skins/ui/oxide/content.min.css';
-import 'tinymce/skins/content/default/content.min.css';
+import { useState } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import './Editor.scss';
+import useViewport from '../../hooks/useViewport';
 
 export default function Editor (props) {
-    const editorRef = useRef(null);
+  const { isMobile } = useViewport();
+  const [editorContent, setEditorContent] = useState('');
 
-    return (
-        <div className="editor-container">
-          <TinyMceEditor 
-            // tinymceScriptSrc='node_modules/tinymce/tinymce.min.js'
-            onInit={(ev, editor) => editorRef.current = editor}
-            initialValue="<p>This is the initial content of the editor.</p>"
-            init={{
-              height: props.height? props.height : 500,
-              menubar: true,
-              plugins: [
-                'advlist autolink lists link image charmap print preview anchor',
-                'searchreplace visualblocks code fullscreen',
-                'insertdatetime media table paste code help wordcount'
-              ],
-              toolbar: 'undo redo | formatselect | ' +
-              'bold italic backcolor | alignleft aligncenter ' +
-              'alignright alignjustify | bullist numlist outdent indent | ' +
-              'removeformat | help',
-              content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-            }}
-            // value={contentEditor}
-            // onEditorChange={this.handleEditorChange}
-          />
-        </div>
-    );
+  return (
+    <div className="editor-container">
+      <CKEditor
+        editor={ ClassicEditor }
+        key={isMobile}
+        config={{
+          toolbar: {
+            viewportTopOffset: isMobile? 45 : 100
+            // viewportTopOffset: 100
+          }
+        }}
+        data={editorContent}
+        onReady={editor => {
+          // You can store the "editor" and use when it is needed.
+          console.log( 'Editor is ready to use!', editor );
+        }}
+        onChange={(event, editor ) => {
+          const data = editor.getData();
+          setEditorContent(data);
+          console.log( { event, editor, data } );
+        }}
+        onBlur={(event, editor ) => {
+          console.log( 'Blur.', editor );
+        }}
+        onFocus={(event, editor ) => {
+          console.log( 'Focus.', editor );
+        }}
+      />
+    </div>
+  );
 }
