4d3682f948593c90b894cfc79dd9f4811d600cb0
Abhra303 <chakrabortyabhradeep79@gmail.com>
2021-07-25 14:41:15 +0530

    enable name and profile-pic keys for orgs in get-token response

--- server/the_water_project/token.py
+++ server/the_water_project/token.py
@@ -1,6 +1,6 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from the_water_project.users.serializers import OnlyIdAndNameOrgSerializer
 
 class UserTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -8,11 +8,11 @@ class UserTokenObtainPairSerializer(TokenObtainPairSerializer):
         data.update({"username": self.user.username})
         data.update({"id": self.user.id})
         if self.user.orgs.count():
-            owned_orgs = [org.id for org in self.user.orgs.all()]
+            owned_orgs = [OnlyIdAndNameOrgSerializer(org).data for org in self.user.orgs.all()]
         else:
             owned_orgs = []
         if self.user.member_of_orgs.count():
-            membered_orgs = [org.id for org in self.user.member_of_orgs.all()]
+            membered_orgs = [OnlyIdAndNameOrgSerializer(org).data for org in self.user.member_of_orgs.all()]
         else:
             membered_orgs = []
         data.update({"owned_orgs": owned_orgs})
