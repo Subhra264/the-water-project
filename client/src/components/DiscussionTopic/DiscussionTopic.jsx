f91bcdc10c98aa945ebb2cfbe6952a7303b60227
Subhra264 <chakrabortysubhradeep556@gmail.com>
2023-01-25 17:11:33 +0530

    client: Fix double slash bug for comments

--- client/src/components/DiscussionTopic/DiscussionTopic.jsx
+++ client/src/components/DiscussionTopic/DiscussionTopic.jsx
@@ -1,200 +1,229 @@
-import { Switch, Route, useParams, Link, Redirect, useHistory } from 'react-router-dom';
-import { useMatchURL } from '../../hooks/useMatch';
-import { useContext, useEffect, useRef, useState } from 'react';
-import useViewport from '../../hooks/useViewport';
-import Slider from '../Slider/Slider';
-import './DiscussionTopic.scss';
-import Issues from './Issues/Issues';
-import Comments from './Comments/Comments';
-import { TopicContext, UserContext } from '../../utils/contexts';
-import ProgressReport from './ProgressReport/ProgressReport';
-import Loader from '../Loader/Loader';
-import Description from './Description/Description';
-import { parseDate } from '../../utils/date';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { getRequest } from '../../utils/fetch-request';
-import { getAccessTokenFromStorage } from '../../utils/manage-tokens';
-import { includesOrg } from '../../utils/user-utils';
-import Contributors from './Contributors/Contributors';
+import {
+  Switch,
+  Route,
+  useParams,
+  Link,
+  Redirect,
+  useHistory,
+} from "react-router-dom";
+import { useMatchURL } from "../../hooks/useMatch";
+import { useContext, useEffect, useRef, useState } from "react";
+import useViewport from "../../hooks/useViewport";
+import Slider from "../Slider/Slider";
+import "./DiscussionTopic.scss";
+import Issues from "./Issues/Issues";
+import Comments from "./Comments/Comments";
+import { TopicContext, UserContext } from "../../utils/contexts";
+import ProgressReport from "./ProgressReport/ProgressReport";
+import Loader from "../Loader/Loader";
+import Description from "./Description/Description";
+import { parseDate } from "../../utils/date";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getRequest } from "../../utils/fetch-request";
+import { getAccessTokenFromStorage } from "../../utils/manage-tokens";
+import { includesOrg } from "../../utils/user-utils";
+import Contributors from "./Contributors/Contributors";
 
 export default function DiscussionTopic(props) {
-    const { isMobile } = useViewport();
-    const [topicDetails, setTopicDetails] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [isClosed, setIsClosed] = useState();
-    const [administratorAccess, setAdministratorAccess] = useState(false);
-    const matchURL = useMatchURL();
-    const { topicId } = useParams();
-    const { userState } = useContext(UserContext);
-    const history = useHistory();
-    
-    // Labels for the Slider component
-    const labels = useRef([
-        {
-            name: 'Description',
-            linkTo: `${matchURL}/description`,
-            ref: useRef()
-        },
-        {
-            name: 'Issues',
-            linkTo: `${matchURL}/issues`,
-            ref: useRef()
-        },
-        {
-            name: 'Progress Report',
-            linkTo: `${matchURL}/progress-report`,
-            ref: useRef()
-        },
-        {
-            name: 'Discussion',
-            linkTo: `${matchURL}/discussion`,
-            ref: useRef()
-        },
-        {
-            name: 'Contributors',
-            linkTo: `${matchURL}/contributors`,
-            ref: useRef()
-        }
-    ]);
+  const { isMobile } = useViewport();
+  const [topicDetails, setTopicDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isClosed, setIsClosed] = useState();
+  const [administratorAccess, setAdministratorAccess] = useState(false);
+  const matchURL = useMatchURL();
+  const { topicId } = useParams();
+  const { userState } = useContext(UserContext);
+  const history = useHistory();
 
-    useEffect(() => {
+  // Labels for the Slider component
+  const labels = useRef([
+    {
+      name: "Description",
+      linkTo: `${matchURL}/description`,
+      ref: useRef(),
+    },
+    {
+      name: "Issues",
+      linkTo: `${matchURL}/issues`,
+      ref: useRef(),
+    },
+    {
+      name: "Progress Report",
+      linkTo: `${matchURL}/progress-report`,
+      ref: useRef(),
+    },
+    {
+      name: "Discussion",
+      linkTo: `${matchURL}/discussion`,
+      ref: useRef(),
+    },
+    {
+      name: "Contributors",
+      linkTo: `${matchURL}/contributors`,
+      ref: useRef(),
+    },
+  ]);
 
-        const successHandler = (result) => {
-            setTopicDetails(result);
-            setIsClosed(result.is_closed);
-            setLoading(false);
-        };
+  useEffect(() => {
+    const successHandler = (result) => {
+      setTopicDetails(result);
+      setIsClosed(result.is_closed);
+      setLoading(false);
+    };
 
-        const errorHandler = (errMessage) => {
-            console.log('Error fetching topic details', errMessage);
-            if (errMessage === 'page_not_found') history.push('/not-found');
-        };
+    const errorHandler = (errMessage) => {
+      console.log("Error fetching topic details", errMessage);
+      if (errMessage === "page_not_found") history.push("/not-found");
+    };
 
-        // Fetch the topic
-        getRequest(`/topics/${topicId}`, getAccessTokenFromStorage(), successHandler, errorHandler);
+    // Fetch the topic
+    getRequest(
+      `/topics/${topicId}`,
+      getAccessTokenFromStorage(),
+      successHandler,
+      errorHandler
+    );
+  }, [topicId]);
 
-    }, [topicId]);
+  useEffect(() => {
+    // Check if the user has administrator access for this topic
+    if (userState && topicDetails) {
+      console.log("");
+      if (
+        topicDetails.description.creator.id === +userState.id ||
+        (topicDetails.creator.org &&
+          (includesOrg(userState.owned_orgs, topicDetails.creator.org.id) ||
+            includesOrg(userState.membered_orgs, topicDetails.creator.org.id)))
+      ) {
+        setAdministratorAccess(true);
+      }
+    }
+  }, [topicDetails, userState]);
 
-    useEffect(() => {
-        // Check if the user has administrator access for this topic
-        if (userState && topicDetails) {
-            console.log('')
-            if (topicDetails.description.creator.id === +userState.id
-                || (topicDetails.creator.org
-                    && (includesOrg(userState.owned_orgs, topicDetails.creator.org.id)
-                        || includesOrg(userState.membered_orgs, topicDetails.creator.org.id)))) {
-                            setAdministratorAccess(true);
-                        }
-        }
-    }, [topicDetails, userState]);
+  return (
+    <div className="discussion-topic-container">
+      {loading ? (
+        <Loader width="7em" />
+      ) : (
+        <>
+          {topicDetails.creator.org && (
+            <div className="associated-ngo">
+              <div className="ngo-logo">
+                {topicDetails.creator.org.profile_pic ? (
+                  <img
+                    src={topicDetails.creator.org.profile_pic}
+                    title="NGO profile pic"
+                    alt="NGO profile pic"
+                    className="profile-pic-user-img"
+                  />
+                ) : (
+                  <FontAwesomeIcon
+                    icon="users"
+                    className="profile-pic-user-circle"
+                  />
+                )}
+              </div>
+              <div className="ngo-name">
+                <Link to={`/discussion/ngos/${topicDetails.creator.org.id}`}>
+                  {topicDetails.creator.org.name}
+                </Link>
+              </div>
+            </div>
+          )}
+          <div className={`discussion-topic ${isMobile ? "mobile" : ""}`}>
+            <div className={`topic-number ${isMobile ? "mobile" : ""}`}>
+              #{topicDetails.id}
+            </div>
+            <div className="topic-details">
+              <div className="topic-title">{topicDetails.title}</div>
+              <div className="topic-date">
+                Opened by{" "}
+                <Link
+                  to={`/discussion/users/${topicDetails.description.creator.id}`}
+                >
+                  <i className="topic-opened-by">
+                    @{topicDetails.description.creator.username}
+                  </i>
+                </Link>{" "}
+                on {parseDate(topicDetails.date)}
+              </div>
+              <div className="topic-labels">
+                <i>Labels:</i>
+                {topicDetails.tags.map((tag) => (
+                  <div className="topic-label" key={tag.id}>
+                    {tag.name}
+                  </div>
+                ))}
+              </div>
+              <div className="topic-location-container">
+                <i>
+                  <FontAwesomeIcon icon="map-marker-alt" />
+                </i>
+                <span className="topic-location">
+                  {topicDetails.city_or_area}, {topicDetails.country}
+                </span>
+              </div>
+              {
+                /* TODO:  */
+                // <Slider>
+                //     <Slider.Label linkTo={`${matchURL}/description`}>Description</Slider.Label>
+                //     <Slider.Label linkTo={`${matchURL}/issues`}>Issues</Slider.Label>
+                //     <Slider.Label linkTo={`${matchURL}/progress-report`}>Progress Report</Slider.Label>
+                //     <Slider.Label linkTo={`${matchURL}/discussion`}>Discussion</Slider.Label>
+                // </Slider>
+              }
 
-    return (
-        <div className='discussion-topic-container'>
-            {
-                loading?
-                    <Loader width='7em' />
-                :
-                    <>
-                        {
-                            topicDetails.creator.org && <div className="associated-ngo">
-                                <div className="ngo-logo">
-                                    {
-                                        topicDetails.creator.org.profile_pic?
-                                            <img 
-                                                src={topicDetails.creator.org.profile_pic} 
-                                                title='NGO profile pic'
-                                                alt='NGO profile pic'
-                                                className='profile-pic-user-img'
-                                            />
-                                        :
-                                            <FontAwesomeIcon icon='users' className='profile-pic-user-circle' />
-                                    }
-                                </div>
-                                <div className="ngo-name"><Link to={`/discussion/ngos/${topicDetails.creator.org.id}`}>{topicDetails.creator.org.name}</Link></div>
-                            </div>
-                        }
-                        <div className={`discussion-topic ${isMobile? 'mobile' : ''}`}>
-                            <div className={`topic-number ${isMobile? 'mobile' : ''}`}>#{topicDetails.id}</div>
-                            <div className="topic-details">
-                                <div className="topic-title">
-                                    {topicDetails.title}
-                                </div>
-                                <div className="topic-date">
-                                    Opened by <Link to={`/discussion/users/${topicDetails.description.creator.id}`}><i className='topic-opened-by'>@{topicDetails.description.creator.username}</i></Link> on {parseDate(topicDetails.date)}
-                                </div>
-                                <div className="topic-labels">
-                                    <i>Labels:</i>
-                                    {
-                                        topicDetails.tags.map(tag => (
-                                            <div className="topic-label" key={tag.id}>{tag.name}</div>
-                                        ))
-                                    }
-                                </div>
-                                <div className="topic-location-container">
-                                    <i><FontAwesomeIcon icon='map-marker-alt' /></i>
-                                    <span className="topic-location">
-                                        {topicDetails.city_or_area}, {topicDetails.country}
-                                    </span>
-                                </div>
-                                {
-                                    /* TODO:  */
-                                // <Slider>
-                                //     <Slider.Label linkTo={`${matchURL}/description`}>Description</Slider.Label>
-                                //     <Slider.Label linkTo={`${matchURL}/issues`}>Issues</Slider.Label>
-                                //     <Slider.Label linkTo={`${matchURL}/progress-report`}>Progress Report</Slider.Label>
-                                //     <Slider.Label linkTo={`${matchURL}/discussion`}>Discussion</Slider.Label>
-                                // </Slider> 
-                                }
-            
-                                <Slider labels={labels.current}/>
-            
-                                <TopicContext.Provider 
-                                    value={{
-                                        topicId: topicDetails.id,
-                                        topicCreator: {
-                                            user: topicDetails.description.user
-                                        },
-                                        administratorAccess
-                                    }}
-                                >
-                                    <Switch>
-                                        <Route path={`${matchURL}/description`} exact>
-                                            {/* <Comment 
+              <Slider labels={labels.current} />
+
+              <TopicContext.Provider
+                value={{
+                  topicId: topicDetails.id,
+                  topicCreator: {
+                    user: topicDetails.description.user,
+                  },
+                  administratorAccess,
+                }}
+              >
+                <Switch>
+                  <Route path={`${matchURL}/description`} exact>
+                    {/* <Comment 
                                                 isDescription 
                                                 {...topicDetails.description}
                                                 baseURI={`/topics/${topicDetails.id}/description`}
                                             /> */}
-                                            <Description 
-                                                description={topicDetails.description}
-                                                baseURI={`/topics/${topicDetails.id}/description`}
-                                                isClosed={isClosed}
-                                                setIsClosed={setIsClosed}
-                                                closeBaseURI='/topics/close-topic'
-                                                problemId={topicDetails.id}
-                                                problemType='Topic'
-                                            />
-                                        </Route>
-                                        <Route path={`${matchURL}/issues`} >
-                                            <Issues />
-                                        </Route>
-                                        <Route path={`${matchURL}/progress-report`} exact>
-                                            <ProgressReport />
-                                        </Route>
-                                        <Route path={`${matchURL}/discussion`} exact>
-                                            <Comments fetchURI={`/topics/${topicDetails.id}/comments/`} />
-                                        </Route>
-                                        <Route path={`${matchURL}/contributors`} exact>
-                                            <Contributors />
-                                        </Route>
-                                        <Route>
-                                            <Redirect to='/not-found' />
-                                        </Route>
-                                    </Switch>
-                                </TopicContext.Provider>
-                            </div>
-                        </div>
-                    </>
-            }
-        </div>
-    );
+                    <Description
+                      description={topicDetails.description}
+                      baseURI={`/topics/${topicDetails.id}/description`}
+                      isClosed={isClosed}
+                      setIsClosed={setIsClosed}
+                      closeBaseURI="/topics/close-topic"
+                      problemId={topicDetails.id}
+                      problemType="Topic"
+                    />
+                  </Route>
+                  <Route path={`${matchURL}/issues`}>
+                    <Issues />
+                  </Route>
+                  <Route path={`${matchURL}/progress-report`} exact>
+                    <ProgressReport />
+                  </Route>
+                  <Route path={`${matchURL}/discussion`} exact>
+                    <Comments
+                      fetchURI={`/topics/${topicDetails.id}/comments`}
+                    />
+                  </Route>
+                  <Route path={`${matchURL}/contributors`} exact>
+                    <Contributors />
+                  </Route>
+                  <Route>
+                    <Redirect to="/not-found" />
+                  </Route>
+                </Switch>
+              </TopicContext.Provider>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
--- client/src/components/DiscussionTopic/Issues/Issue/Issue.jsx
+++ client/src/components/DiscussionTopic/Issues/Issue/Issue.jsx
@@ -1,109 +1,131 @@
-import Comments from '../../Comments/Comments';
+import Comments from "../../Comments/Comments";
 // import Comment from '../../Comments/Comment/Comment';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useEffect, useState, useRef, useContext } from 'react';
-import './Issue.scss';
-import useViewport from '../../../../hooks/useViewport';
-import { TopicContext } from '../../../../utils/contexts';
-import Loader from '../../../Loader/Loader';
-import Description from '../../Description/Description';
-import { parseDate } from '../../../../utils/date';
-import { getRequest } from '../../../../utils/fetch-request';
-import { getAccessTokenFromStorage } from '../../../../utils/manage-tokens';
-import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState, useRef, useContext } from "react";
+import "./Issue.scss";
+import useViewport from "../../../../hooks/useViewport";
+import { TopicContext } from "../../../../utils/contexts";
+import Loader from "../../../Loader/Loader";
+import Description from "../../Description/Description";
+import { parseDate } from "../../../../utils/date";
+import { getRequest } from "../../../../utils/fetch-request";
+import { getAccessTokenFromStorage } from "../../../../utils/manage-tokens";
+import { Link } from "react-router-dom";
 
-function IssueThread (props) {
-    const [issueDescription, setIssueDescription] = useState({});
-    const [loading, setLoading] = useState(true);
-    const [isClosed, setIsClosed] = useState(props.isClosed);
-    const { topicId } = useContext(TopicContext);
+function IssueThread(props) {
+  const [issueDescription, setIssueDescription] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [isClosed, setIsClosed] = useState(props.isClosed);
+  const { topicId } = useContext(TopicContext);
 
-    useEffect(() => {
-
-        const successHandler = (result) => {
-            setIssueDescription(result);
-            setLoading(false);
-        };
-
-        const errorHandler = (errMessage) => {
-            console.log('Error fetching issue description', errMessage);
-        };
-
-        // Fetch the description api
-        getRequest(
-            `/topics/${topicId}/issues/${props.issueId}/description/`,
-            getAccessTokenFromStorage(),
-            successHandler,
-            errorHandler
-        );
-    }, []);
-
-    return (
-        <div className={`discussion-topic-issue-description ${props.display}`}>
-            {
-                loading? 
-                    <Loader width='3em' />
-                :
-                    <>
-                        <Description
-                            description={issueDescription}
-                            baseURI={`/topics/${topicId}/issues/${props.issueId}/description`}
-                            isClosed={isClosed}
-                            setIsClosed={setIsClosed}
-                            closeBaseURI={`/topics/${topicId}/issues/close-issue`}
-                            problemId={props.issueId}
-                            problemType='Issue'
-                        />
-                        <Comments fetchURI={`/topics/${topicId}/issues/${props.issueId}/comments/`} />
-                    </>
-            }
-        </div>
-    );
-}
-
-export default function Issue (props) {
-    const { isMobile } = useViewport();
-    const [showComments, setShowComments] = useState(false);
-    const shouldLoadComment = useRef(false);
-
-    const toggleShowComments = () => {
-        if (!shouldLoadComment.current) shouldLoadComment.current = true;
-        setShowComments(!showComments);
+  useEffect(() => {
+    const successHandler = (result) => {
+      setIssueDescription(result);
+      setLoading(false);
     };
 
-    return (
-        <div className="discussion-topic-issue">
+    const errorHandler = (errMessage) => {
+      console.log("Error fetching issue description", errMessage);
+    };
 
-            <div className={`discussion-topic-issue-header ${isMobile? 'mobile' : ''}`}>
-                <div className="discussion-topic-issue-is-closed">
-                    <FontAwesomeIcon icon={['far', `${props.is_closed? 'check-circle' : 'dot-circle'}`]} />&nbsp;
-                    {isMobile? '' : props.is_closed? 'Closed' : 'Open'}
-                </div>
-                <div className="discussion-topic-issue-title">
-                    <div className="issue-title">{props.title}</div>
-                    <div className="issue-meta-data">
-                        <span>opened by <Link to={`/discussion/users/${props.creator.user.id}`} ><i>@{props.creator.user.username}</i></Link> on {parseDate(props.date)} </span>
-                        <span className='issue-tags'>
-                            {
-                                props.tags.map(tag => (
-                                    <span className="issue-tag" key={tag.id}>{tag.name}</span>
-                                ))
-                            }
-                        </span>
-                    </div>
-                </div>
-                <div className="discussion-topic-issue-meta">
-                    <div className="discussion-topic-issue-meta-data" title='No of comments'>
-                        <FontAwesomeIcon icon='comments' /> {props.no_of_comments}
-                    </div>
-                    <div className="discussion-topic-issue-meta-data" onClick={toggleShowComments} >
-                        <FontAwesomeIcon icon={`${showComments? 'chevron-up' : 'chevron-down'}`} />
-                    </div>
-                </div>
-            </div>
-            {
-                shouldLoadComment.current && <IssueThread display={showComments? '' : 'display-none'} issueId={props.id} isClosed={props.is_closed} />
-            }
+    // Fetch the description api
+    getRequest(
+      `/topics/${topicId}/issues/${props.issueId}/description/`,
+      getAccessTokenFromStorage(),
+      successHandler,
+      errorHandler
+    );
+  }, []);
+
+  return (
+    <div className={`discussion-topic-issue-description ${props.display}`}>
+      {loading ? (
+        <Loader width="3em" />
+      ) : (
+        <>
+          <Description
+            description={issueDescription}
+            baseURI={`/topics/${topicId}/issues/${props.issueId}/description`}
+            isClosed={isClosed}
+            setIsClosed={setIsClosed}
+            closeBaseURI={`/topics/${topicId}/issues/close-issue`}
+            problemId={props.issueId}
+            problemType="Issue"
+          />
+          <Comments
+            fetchURI={`/topics/${topicId}/issues/${props.issueId}/comments`}
+          />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default function Issue(props) {
+  const { isMobile } = useViewport();
+  const [showComments, setShowComments] = useState(false);
+  const shouldLoadComment = useRef(false);
+
+  const toggleShowComments = () => {
+    if (!shouldLoadComment.current) shouldLoadComment.current = true;
+    setShowComments(!showComments);
+  };
+
+  return (
+    <div className="discussion-topic-issue">
+      <div
+        className={`discussion-topic-issue-header ${isMobile ? "mobile" : ""}`}
+      >
+        <div className="discussion-topic-issue-is-closed">
+          <FontAwesomeIcon
+            icon={["far", `${props.is_closed ? "check-circle" : "dot-circle"}`]}
+          />
+          &nbsp;
+          {isMobile ? "" : props.is_closed ? "Closed" : "Open"}
         </div>
-    )
+        <div className="discussion-topic-issue-title">
+          <div className="issue-title">{props.title}</div>
+          <div className="issue-meta-data">
+            <span>
+              opened by{" "}
+              <Link to={`/discussion/users/${props.creator.user.id}`}>
+                <i>@{props.creator.user.username}</i>
+              </Link>{" "}
+              on {parseDate(props.date)}{" "}
+            </span>
+            <span className="issue-tags">
+              {props.tags.map((tag) => (
+                <span className="issue-tag" key={tag.id}>
+                  {tag.name}
+                </span>
+              ))}
+            </span>
+          </div>
+        </div>
+        <div className="discussion-topic-issue-meta">
+          <div
+            className="discussion-topic-issue-meta-data"
+            title="No of comments"
+          >
+            <FontAwesomeIcon icon="comments" /> {props.no_of_comments}
+          </div>
+          <div
+            className="discussion-topic-issue-meta-data"
+            onClick={toggleShowComments}
+          >
+            <FontAwesomeIcon
+              icon={`${showComments ? "chevron-up" : "chevron-down"}`}
+            />
+          </div>
+        </div>
+      </div>
+      {shouldLoadComment.current && (
+        <IssueThread
+          display={showComments ? "" : "display-none"}
+          issueId={props.id}
+          isClosed={props.is_closed}
+        />
+      )}
+    </div>
+  );
 }
